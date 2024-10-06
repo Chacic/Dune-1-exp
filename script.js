95e70672cc5042b66b63bf3d4e5980db43c3cbd6
Chacic <133131541+Chacic@users.noreply.github.com>
2024-10-06 22:11:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-// JavaScript for Menu Animation, Scroll Behavior, and Animations on Scroll
+// JavaScript for Menu Animation and Animations on Scroll
 
 // Variables
-const header = document.querySelector('header');
 const navLinks = document.querySelector('.nav-links');
 const menuIcon = document.querySelector('.menu-icon');
-let lastScrollY = window.scrollY;
 
 // Toggle Mobile Menu
 menuIcon.addEventListener('click', () => {
   navLinks.classList.toggle('nav-active');
-});
-
-// Hide Header on Scroll Down
-window.addEventListener('scroll', () => {
-  if (window.scrollY > lastScrollY && window.scrollY > 100) {
-    // Hide header after scrolling down more than 100px
-    header.classList.add('header-hidden');
-  } else {
-    header.classList.remove('header-hidden');
-  }
-  lastScrollY = window.scrollY;
 });
 
 // Fade-In Animations
